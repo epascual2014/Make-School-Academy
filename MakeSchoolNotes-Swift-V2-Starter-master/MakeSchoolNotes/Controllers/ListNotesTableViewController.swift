1b7b08daa1b6e5a62a1cac7e965b6756780a518c
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-06-21 23:25:00 -0700

    Setup TableView cells and identifier
    Cells and identifier added.

--- MakeSchoolNotes-Swift-V2-Starter-master/MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes-Swift-V2-Starter-master/MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,8 +10,27 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        
+    }
   
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "Yay, its working"
+        
+        return cell
+        
+    }
+    
+    
+//    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        <#code#>
+//    }
 }
