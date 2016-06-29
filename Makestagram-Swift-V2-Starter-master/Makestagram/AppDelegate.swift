298a230d9d8870dc7aa23dbfa68bcd91bb11887c
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-06-29 00:55:21 -0700

    Displaying images
    -Setup custom table View
    -Connected custom class to the table view cell
    -Outlet for the UIImageView
    -Code to display images
    -Downloading PFFiles
    -Updated Info.plist

--- Makestagram-Swift-V2-Starter-master/Makestagram/AppDelegate.swift
+++ Makestagram-Swift-V2-Starter-master/Makestagram/AppDelegate.swift
@@ -38,6 +38,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print ("No logged in user :(")
         }
 
+        // MARK: ACCESS CONTROL LISTS
+        let acl = PFACL()
+        acl.publicReadAccess = true
+        PFACL.setDefaultACL(acl, withAccessForCurrentUser: true)
+        
         return true
     }
 
--- Makestagram-Swift-V2-Starter-master/Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram-Swift-V2-Starter-master/Makestagram/ViewControllers/TimelineViewController.swift
@@ -13,18 +13,64 @@ var photoTakingHelper: PhotoTakingHelper?
 
 class TimelineViewController: UIViewController {
 
+    
+    var posts: [Post] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tabBarController?.delegate = self
     }
     
+    override func viewDidAppear(animated: Bool){
+        super.viewDidAppear(animated)
+        
+        // Follow relationships query for current user
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo: PFUser.currentUser()!)
+    
+        // Fetch posts from Followed Users
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+    
+        // Query to retrieve all posts from current user
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+    
+        // Combines 2nd and 3rd queries to return any posts.
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        
+        // Fetch PFUser
+        query.includeKey("user")
+        // Results are ordered by descending order
+        query.orderByDescending("createdAt")
+        
+        query.findObjectsInBackgroundWithBlock {(result: [PFObject]?, error: NSError?) -> Void in
+            
+            // Received all posts in an array.
+            self.posts = result as? [Post] ?? []
+            
+            for post in self.posts {
+                do {
+                    let data = try post.imageFile?.getData()
+                    post.image = UIImage(data: data!, scale: 1.0)
+                    
+                } catch {
+                    print("could not get image")
+                }
+            }
+            
+            self.tableView.reloadData()
+        }
+    }
     
 }
 
 // MARK: Tab Bar Delegate
 
-extension TimelineViewController: UITabBarControllerDelegate {
+extension TimelineViewController: UITabBarControllerDelegate, UITableViewDataSource {
     
     func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
         
@@ -45,6 +91,20 @@ extension TimelineViewController: UITabBarControllerDelegate {
             post.image = image
             post.uploadPost()
         }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+
+        cell.postImageView.image = posts[indexPath.row].image
+        
+        return cell
     }
 }
 //        
