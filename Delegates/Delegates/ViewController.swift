feaa6e11bb34784388e46376f88c6f6d0c3f1a7e
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-06-30 10:35:24 -0700

    Delegates
    Example of using tags.
    Used pickerselector delegate and datasource.

--- Delegates/Delegates/ViewController.swift
+++ Delegates/Delegates/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        print(#function)
+        
+        // Using Tags for selecting which textField to use.
+        if textField.tag == 1 {
+            print("1")
+            
+        } else if textField.tag == 2 {
+             print("2")
+            
+        }
+    }
+    
+    
 
+    
+    
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let cities = ["city1","city2", "city3"]
+
+    
+    @IBOutlet weak var textFieldOne: UITextField!
+    
+    @IBOutlet weak var textFieldTwo: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +54,20 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.cities.count 
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return cities[row]
+    }
+    
+    
+    
+}
